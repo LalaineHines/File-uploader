964ea0b0a15967dd66cd3cc358af49c527a79277
Lalaine Hines <150487195+LalaineHines@users.noreply.github.com>
2025-08-31 20:19:26 +0000

    added app script for functionality of page

---
+++ app.js
@@ -0,0 +1,80 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const  { PrismaClient } = require('@prisma/client');
+const PrismaStore = require('@quixo3/prisma-session-store').PrismaSessionStore;
+const bcrypt = require('bcrypt');
+const path = require('path');
+const engine = require('ejs-mate');
+const setLocals = require('./middleware/setLocals');
+
+// ROUTES
+const authRoute = require('./routes/auth');
+const dashboardRoutes = require('./routes/dashboard');
+const folderRoutes = require('./routes/folders');
+const fileRoutes = require('./routes/files');
+const sharedRoutes = require('./routes/share');
+
+
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.stratic(path.join(__dirname, 'public')));
+
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// SESSION
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaStore(prisma, {
+        checkPeriod: 2 * 60 * 1000,
+        dbRecordIdIsSessionId: true,
+    }),
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }
+}));
+
+// PASSPORT
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(setLocals);
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+    const user = await prisma.user.findUnique({ where: { id } });
+    done(null, user);
+});
+
+passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+    const user = await prisma.user.findUnique({ where: { email} });
+    if (!user) return done(null, false, { message: "User not found" });
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return done(null, false, { message: "Wrong password" });
+
+    return done(null, user);
+}));
+
+// PUBLIC PAGE
+app.get('/', (req, res) => {
+    res.render('index',  {user: req.user });
+});
+
+// ROUTE SETUP
+app.use(authRoutes);
+app.use(dashboardRoutes);
+app.use('/folders', folderRoutes);
+app.use('/files', fileRoutes);
+app.use(shareRoutes);
+
+// START
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
