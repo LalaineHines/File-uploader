5c5c6fd69c8ed1b0902aca58612ca7dedd55a10b
Lalaine Hines <150487195+LalaineHines@users.noreply.github.com>
2025-08-31 20:56:37 +0000

    added script for authentication control

---
+++ controllers/authController.js
@@ -0,0 +1,28 @@
+const { PrismaClient } = require('@prisma/client');
+const bycrypt = require('bycrypt');
+const prisma = new PrismaClient();
+
+exports.registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    const existing = await prisma.user.findUnique({ where: { email} });
+    if (existing) return res.send('User already exists');
+
+    const hashed = await bycrypt.hash(password, 10);
+    await prisma.user.create({ data: { name, email, password: hashed } });
+
+    res.redirect('/login');
+};
+
+exports.loginPage = (req, res) => {
+    res.render('login');
+};
+
+exports.registerPage = (req, res) => {
+    res.render('register');
+};
+
+exports.logoutUser = (req, res) => {
+    req.logout(() => {
+        res.redirect('/login');
+    });
+};
